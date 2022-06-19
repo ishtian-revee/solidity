d594c3ee5df6f95daa8620a0529973bb6c798541
ishtian revee <ishtian11@gmail.com>
2022-06-20 00:27:50 +0600

    - notes for memory, calldata and storage added

--- solidity/3_struct_and_array.sol
+++ solidity/3_struct_and_array.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage {
     
-    uint256 favoriteNumber;
+    uint256 favoriteNumber;     // automatically cast to be a Storage variable
 
     struct People {
         uint256 number;
@@ -16,6 +16,8 @@ contract SimpleStorage {
     People[] public people;     // Dynamic array: the size of the array is not predefined
     People[3] public people1;   // array size given
 
+    // if variable of type array, struct and mapping then it needs to be specified as memory
+    // string is basically array of bytes, this is why it is specified as memory
     function addPerson(string memory _name, uint256 _number) public {
         people.push(People(_number, _name));
     }
@@ -29,3 +31,25 @@ contract SimpleStorage {
         return favoriteNumber;
     }
 }
+
+/*
+
+Notes:
+
+EVM can access and store information in six places:
+
+1. Stack
+2. Memory
+3. Storage
+4. Calldata
+5. Code
+6. Logs
+
+- variables can be in only Memory, Storage and Calldata
+- Calldata are temporary variables that are immutable or cannot be modified
+- Memory are temporary variables that mutable or can be modified
+- Storage is permanent variables that can be modified
+- Calldata and Memory variable has access to a specific function
+- Storage even has access outside of the function
+
+*/
